a9583246643102380fd997ff5decef5bd47b6ebf
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-11-07 20:59:19 +0800

    Hamburger Update

--- Overview/overview.js
+++ Overview/overview.js
@@ -1,14 +1,22 @@
 const hamburger = document.getElementById('hamburger');
 const sideMenu = document.getElementById('sidemenu');
 const closeMenu = document.getElementById('closeMenu');
+const overlay = document.getElementById('overlay');
 
-hamburger.addEventListener('click', () => {
+function openMenu(){
     sideMenu.classList.add('active');
     setTimeout(() => {
         hamburger.style.display = 'none';
     }, 350);
-});
-closeMenu.addEventListener('click', () => {
+    overlay.classList.add('active');
+}
+function closeMenuFunc(){
     sideMenu.classList.remove('active');
-    hamburger.style.display = 'flex';
-});
+    overlay.classList.remove('active');
+    setTimeout(() => {
+        hamburger.style.display = 'flex';
+    }, 100);
+}
+hamburger.addEventListener('click', openMenu);
+closeMenu.addEventListener('click', closeMenuFunc);
+overlay.addEventListener('click', closeMenuFunc);  
