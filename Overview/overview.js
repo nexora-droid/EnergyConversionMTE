be784b5f646515e3bf568e2aa3329b220bd68853
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-11-11 14:10:07 +0800

    Added quick info table, and animations for image on page

--- Overview/overview.js
+++ Overview/overview.js
@@ -20,3 +20,37 @@ function closeMenuFunc(){
 hamburger.addEventListener('click', openMenu);
 closeMenu.addEventListener('click', closeMenuFunc);
 overlay.addEventListener('click', closeMenuFunc);  
+
+const scrollElement = document.querySelectorAll('.animimg');
+
+scrollElement.forEach((el) => {
+    el.style.transform = 'translateY(100px) rotate(0deg)';
+});
+const elementInView = (el, percentageScroll=100) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return (
+        elementTop <= (window.innerHeight || document.documentElement.clientHeight * (percentageScroll/100))
+    );
+}
+
+const displayScrollElement = (elment) => {
+    elment.classList.add('scrolled');
+
+    setTimeout(() => {
+        elment.style.transition = 'transform 800ms ease-out';
+        elment.style.transform = 'translateY(0) rotate(-11.66deg)';
+    }, 100);
+};
+const handleScrollAnimation = () => {
+    scrollElement.forEach((el) => {
+        if(elementInView(el, 200)){
+            displayScrollElement(el);
+        }
+    })
+};
+const hideScrollElement = (elment) => {
+    elment.classList.remove('scrolled');
+}
+window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+});
