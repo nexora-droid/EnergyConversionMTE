309a113de71f7ebf9e925e0009b471e3cf9df515
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-11-15 00:18:10 +0800

    PHOTOS PAGE FINISHED ACTUALLY

---
+++ Photos/photos.js
@@ -0,0 +1,22 @@
+const hamburger = document.getElementById('hamburger');
+const sideMenu = document.getElementById('sidemenu');
+const closeMenu = document.getElementById('closeMenu');
+const overlay = document.getElementById('overlay');
+
+function openMenu(){
+    sideMenu.classList.add('active');
+    setTimeout(() => {
+        hamburger.style.display = 'none';
+    }, 350);
+    overlay.classList.add('active');
+}
+function closeMenuFunc(){
+    sideMenu.classList.remove('active');
+    overlay.classList.remove('active');
+    setTimeout(() => {
+        hamburger.style.display = 'flex';
+    }, 350);
+}
+hamburger.addEventListener('click', openMenu);
+closeMenu.addEventListener('click', closeMenuFunc);
+overlay.addEventListener('click', closeMenuFunc);  
